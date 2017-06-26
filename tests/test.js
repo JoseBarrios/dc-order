c02c63448b5b2f86cd032d83cc86b3a4cca56149
Jose Barrios <jose@cognilab.com>
2017-06-26 09:19:36 -0700

    added Order.key test

--- tests/test.js
+++ tests/test.js
@@ -8,8 +8,15 @@ model.name = 'jose';
 model.orderNumber = 1234;
 let order = new Order(model);
 
-describe('person.givenName', function() {
-	it('should capitalize given name', function() {
-		assert.equal(order.name, model.name);
+describe('Order.isNumber', function() {
+	it('should inheret Thing static method isNumber', function() {
+		assert.equal(Order.isNumber(1), true);
+		assert.equal(Order.isNumber('x'), false);
+	});
+});
+
+describe('Order.keys', function() {
+	it('should inheret Thing static method keys', function() {
+		assert.equal(Order.keys(order), true);
 	});
 });
